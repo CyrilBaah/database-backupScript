fbbae44658f87f0e6cba6abe934608b5c85ad105
CyrilBaah <cyril.baah@amalitech.org>
2023-02-08 22:36:42 +0000

    refactor rds_db_backup

--- rds_db_backup.sh
+++ rds_db_backup.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# DATABASE_NAME=realtimeapp
-# DB_USERNAME=root
-# DB_PASSWORD=password123 
-# BUCKET_NAME=amalitech-db-backup
-# BACKUP_DIRECTORY 
-
-# MYSQL_HOST=arms-db.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com
-# MYSQL_PORT=3306
-# MYSQL_USER=root
-# MYSQL_PASSWORD=amalitech
-# DATABASE_NAME=realtimeapp
-
-# MYSQL_HOST=arms-db.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com
-# MYSQL_PORT=3306
-# MYSQL_USER=admin
-# MYSQL_PASSWORD=amalitech
-# DATABASE_NAME=realtimeapp
-
 # Load environment variables from .env file
 while read -r line || [[ -n "$line" ]]; do
   if [[ $line =~ ^[A-Za-z_]+=[^\#]+ ]]; then
@@ -28,27 +10,8 @@ done < .env
 # Set the backup file name
 BACKUP_FILE=$DATABASE_NAME"backup_online_$(date +%Y-%m-%d_%H-%M-%S).sql"
 
-echo $MYSQL_HOST
-echo $MYSQL_PORT
-echo $MYSQL_USER
-echo $MYSQL_PASSWORD
-echo $DATABASE_NAME
-
-
-
-
 # Create the backup file
-# mysqldump -u $DB_USERNAME -p$DB_PASSWORD $DATABASE_NAME > $BACKUP_FILE
-# mysql -h arms-db.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com -P 3306 -u admin -p
- ####
-# mysql -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE_NAME
-# mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD --single-transaction $DATABASE_NAME > $BACKUP_FILE
 mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE_NAME > $BACKUP_FILE
-
-# mysqldump -h arms-db.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com -P 3306 -u admin -p realtimeapp
-
-
-# mysql -h arms-db.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com -P 3306 -u admin -p realtimeap
 
 # Check if the backup file was created successfully
 if [ $? -eq 0 ]; then
